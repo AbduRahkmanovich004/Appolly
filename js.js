310a7f0f12815edad75139678002823fddcfa076
Abu <xabzarbek@gmail.com>
2023-09-26 09:55:48 +0500

    ikkinchi versiya

--- js.js
+++ js.js
@@ -1,22 +1,29 @@
-const downloud =  document.querySelector(".downloud")
-const times = document.querySelector(".times")
-downloud.addEventListener("click",()=>{
-    document.querySelector(".modal").style.display=("flex")
-    document.querySelector("aside").classList.remove("aside")
-})
-times.addEventListener("click",()=>{
-    document.querySelector(".modal").style.display=("none")
-})
-document.addEventListener("keydown",(e)=>{
-    if(e.key=="Escape") document.querySelector(".modal").style.display=("none")
-})
+const downloud = document.querySelector(".downloud");
+const times = document.querySelector(".times");
+const bgModal = document.querySelector(".modalBg");
+downloud.addEventListener("click", () => {
+  document.querySelector(".modal").style.display = "flex";
+  bgModal.classList.remove("hidden");
+});
+bgModal.addEventListener("click", () => {
+  document.querySelector(".modal").style.display = "none";
+  bgModal.classList.add("hidden");
+  document.querySelector("aside").classList.remove("sur");
+});
+document.addEventListener("keydown", (e) => {
+  if (e.key == "Escape") {
+    document.querySelector(".modal").style.display = "none";
+    bgModal.classList.add("hidden");
+  }
+});
 
-
-const menuOpen = document.querySelector(".bottomNav svg")
-const menuClose = document.querySelector("aside img")
-menuOpen.addEventListener("click",()=>{
-    document.querySelector("aside").classList.add("aside")
-})
-menuClose.addEventListener("click",()=>{
-    document.querySelector("aside").classList.remove("aside")
-})
+const menuOpen = document.querySelector(".bottomNav svg");
+const menuClose = document.querySelector("aside img");
+menuOpen.addEventListener("click", () => {
+  document.querySelector("aside").classList.add("sur");
+  bgModal.classList.remove("hidden");
+});
+menuClose.addEventListener("click", () => {
+  document.querySelector("aside").classList.remove("sur");
+  bgModal.classList.add("hidden");
+});
